95bc25d104ec3634dc3182478ee810191db73899
neelamlonari <neelam.lonari@hotmail.com>
2022-12-12 16:41:53 -0500

    Wild Wild Life Answered

--- homework/answer.js
+++ homework/answer.js
@@ -2,7 +2,7 @@
 /*let x=" ";
 for(let i=1; i<=20;i++){
     x = x + (i).toString() + ",";
-  }  console.log(x);*/
+  }  console.log(x);
 
   let x=" ";
   for( let i=1;i<=200;i++){
@@ -11,4 +11,45 @@ for(let i=1; i<=20;i++){
  
    }}
     console.log(x);
+    /////////fizzbuzz///////
+ let x="";
+for( let i = 1; i<=30;i++ ) {
+  
+  
+   if( i % 3===0 && i % 5===0){
+    x=x+"fizzbuzz,";
+   }else if( i%3===0){
+    x=x+"fizz";
+   }else if (i%5===0){
+    x=x+"buzz,";
+   }else{
+    x = x + i+",";
+  }
+
+  }
+  console.log(x);*/
+  ///////////wild life/////
+class Wildlife{
+  costructor(name,species ,age, hometown){
+
+  }
+}
+
+  const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+function birthday(input){
+  input[2]= input[2]+1;
+}
+birthday(plantee);
+console.log(plantee);
+wolfy[3]="Gotham City";
+console.log(wolfy);  
+dart.push("Hawkins");
+console.log(dart);
+wolfy[0]= "Gameboy";
+console.log(wolfy) ;
+
   
